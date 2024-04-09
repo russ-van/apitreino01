8e833aef323b4f4187391400c2c213c5beb43551
Aluno <aluno@edu.senac.br>
2024-04-09 20:50:12 -0300

    09/04/2024

---
+++ apiflask/poo/conta.py
@@ -0,0 +1,18 @@
+#classe é um conjunto de objetos
+class Conta:
+    #O que classe tem?
+    """
+    Objetos
+    Construtor
+    Atributos
+    Funções (métodos)
+        get
+        set
+
+
+    """
+    def ___init___(self,id,descricao,valor):
+        self.id = id
+        self.descricao = descricao
+        self.valor = valor
+        
---
+++ apiflask/poo/pessoa.py
@@ -0,0 +1,6 @@
+class People:
+
+    def ___init___(self, nome, idade,cpf=None):
+        self.nome = nome
+        self.idade = idade
+        self.cpf = cpf
